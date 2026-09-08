3710f475a2fe76fd8c97a56fff5614d372cad3db
pinkleather221 <nellymogere2002@gmail.com>
2026-09-09 00:55:25 +0300

    add initial task1.py with basic print and input examples

---
+++ Day1/task1.py
@@ -0,0 +1,26 @@
+#printing text to output in the console , this is the first line of code every developer wrote first 
+print("Hello World!")
+
+# String manipulation ,,,using \n to add another line of code 
+print("Hello World\n" "Hello world\n" "Hello world")
+
+#concatenation,, we use + to join two strings 
+print("Hello" + " " + "World")
+
+#Input function , this allows to collect user input instead of 
+# printing the same thing again and again like the print function
+input("Enter your name ")
+
+# using the input the user provided us with,,,, we use the + to concatenate
+print("Hello" + input("Enter Your name ") )
+
+# Variables ,,, these act like containers to store data ,,hence we can easily refference it 
+name = input("Enter your name ")
+print(name)
+
+# in order to know the length of a string we use the len function
+# the output will be a number of characters in the name which is 5
+name = "Nelly"
+print(len(name))
+
+
