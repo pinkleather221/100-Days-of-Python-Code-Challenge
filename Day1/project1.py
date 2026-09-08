ecbf253bba97ea69735a5f6cc240fc16216d5de3
pinkleather221 <nellymogere2002@gmail.com>
2026-09-09 01:08:25 +0300

    add band name generator project

---
+++ Day1/project1.py
@@ -0,0 +1,9 @@
+#this project is a band name generator , it gets from the use the city and pet name 
+#then combines the two and outputs it to the user 
+
+print("*" * 50)
+print("Welcome to the band name generator")
+print("*" * 50)
+city = input("Enter the name of your city:\n ")
+pet_name = input ("Enter the name of your pet:\n ")
+print ("Your Band name is " + city + " " + pet_name )
