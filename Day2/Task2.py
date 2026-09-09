8776a5cf29a617eeaaf0a0af4b7b7360fbc5a957
pinkleather221 <nellymogere2002@gmail.com>
2026-09-09 16:57:45 +0300

    add Task2.py with examples of data types, subscripting, and type casting

---
+++ Day2/Task2.py
@@ -0,0 +1,16 @@
+#data types:string , float, integers, boolean
+
+# subscripting,,, extracting characters from a string
+print("Nelly"[0]) # prints the first character
+print("Nelly"[-1]) # prints the last character
+
+# checking the data type
+print(type("Nelly"))
+
+# type casting or converting from data type to another
+int("2345")  #converting the string to integer 
+
+# type error happens when trying to work with the wrong data type eg concatnating a string and an int 
+# f-string ,,,, used to combine variables and strings when printing out 
+name = "Nelly"
+print(f"My name is: {name}")
