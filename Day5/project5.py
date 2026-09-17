b46ef50d31bd5de1a3bc8ff660b4e64bd972f096
pinkleather221 <nellymogere2002@gmail.com>
2026-09-17 16:24:58 +0300

    refactor project5.py to improve input prompts and enhance password generation logic

--- Day5/project5.py
+++ Day5/project5.py
@@ -1,5 +1,5 @@
 # password generator project
-
+import random
 # create lists to store the combiations the password can be made from 
 # all caps and small case letters
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -12,10 +12,21 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';',
 
 print("HELLO! Welcome to your favourite  password generator\n")
 # prompt the user for the number of letters, numbers and symbols they would like their password to have 
-num_letters = int(input("Enter the number of letters you would like your password to have"))
-num_numbers = int(input("How many numbers would like your password to have"))
-num_symbols = int(input("Enter the number of symbols you would like your password to have"))
+num_letters = int(input("Enter the number of letters you would like your password to have\n"))
+num_numbers = int(input("How many numbers would like your password to have\n"))
+num_symbols = int(input("Enter the number of symbols you would like your password to have\n"))
 
-password = ""
-for letter in letters:
-    pass
+#contains the functionality of creating an empty list and then randomly picking the items from the list based by the number input from the user
+# then appending the random items to the empty password list
+password = []
+for letter in range(0,num_letters):
+    random_letter =random.choice(letters)
+    password.append(random_letter)
+
+for num in range(0, num_numbers):
+    random_num = random.choice(numbers)
+    password.append(random_num)
+
+for sym in range(0, num_symbols): 
+    random_symbol = random.choice(symbols)
+    password.append(random_symbol)
