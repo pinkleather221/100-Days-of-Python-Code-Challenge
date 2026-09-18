7a6bdf2a6bd0981d06536717c7e4f7489c37dc9f
pinkleather221 <nellymogere2002@gmail.com>
2026-09-18 09:20:44 +0300

    add example of infinite loop using while statement in Task6.py

---
+++ Day6/Task6.py
@@ -0,0 +1,17 @@
+# functions and while loops(used when you want to loop through
+#  a program when the condition is true , 
+# only making an exi when its false this can be dangerous as the loop will run infinitely if the condition will never be false)
+while 5 > 2:
+    print("I will run forever")
+
+#because 5 will forever be greater than 2 we will get an output of 
+# I will run forever
+# I will run forever
+# I will run forever
+# I will run forever
+# I will run forever
+# I will run forever
+# I will run forever
+# I will run forever
+
+
