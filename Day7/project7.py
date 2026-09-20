371a6751dec4955efc571979aee68109a7efa71d
pinkleather221 <nellymogere2002@gmail.com>
2026-09-20 18:10:46 +0300

    refactor Hangman game logic to improve letter guessing feedback and display

--- Day7/project7.py
+++ Day7/project7.py
@@ -4,9 +4,19 @@ import random
 word_list = ["serendipity", "ephemeral", "luminescence", "solitude", "resilience", "eloquence", "melancholy", "aurora", "petrichor", "alcyon"] 
 random_word = random.choice(word_list)
 print (random_word)
+
+placeholder = ""
+for l in random_word:
+    placeholder += "_"
+print(placeholder)
+
+display = list(placeholder)
+
 guess = input("Guess a letter ").lower()
-for g in random_word:
-    if g == guess:
-        print("Right") 
-    else:
-        print("Wrong")
+
+for g, letter in enumerate(random_word):
+    if letter == guess:
+        display[g] = guess
+        final_display = "".join(display)
+print(final_display)        
+
