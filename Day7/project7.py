1893a7d729acd40ec9d882ac170a03c38d063f25
pinkleather221 <nellymogere2002@gmail.com>
2026-09-20 18:51:13 +0300

    refactor Hangman game to improve display logic for guessed letters

--- Day7/project7.py
+++ Day7/project7.py
@@ -5,18 +5,19 @@ word_list = ["serendipity", "ephemeral", "luminescence", "solitude", "resilience
 random_word = random.choice(word_list)
 print (random_word)
 
+# printing out underscore in place of the word the computer randomly chose 
 placeholder = ""
 for l in random_word:
     placeholder += "_"
 print(placeholder)
 
-display = list(placeholder)
-
+# replacing the underscore with the correct guessed letter 
 guess = input("Guess a letter ").lower()
-
-for g, letter in enumerate(random_word):
+display = ""
+for letter in random_word:
     if letter == guess:
-        display[g] = guess
-        final_display = "".join(display)
-print(final_display)        
+        display += letter
+    else:
+        display += "_"
+print(display)      
 
