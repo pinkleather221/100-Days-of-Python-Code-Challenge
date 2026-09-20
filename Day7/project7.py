7b2cea09817bb5b77186eb13f0966ae5af1e70e8
pinkleather221 <nellymogere2002@gmail.com>
2026-09-20 15:13:05 +0300

    add initial implementation of Hangman game with word list and random selection

---
+++ Day7/project7.py
@@ -0,0 +1,5 @@
+# Hang man game
+# import the random module 
+import random
+word_list = ["Serendipity", "Ephemeral", "Luminescence", "Solitude", "Resilience", "Eloquence", "Melancholy", "Aurora", "Petrichor", "Halcyon"] 
+random_word = random.choices()
