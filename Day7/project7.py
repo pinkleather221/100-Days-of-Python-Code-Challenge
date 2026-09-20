6578db5ca927f5deb6bba69db2b6ada4d4c15446
pinkleather221 <nellymogere2002@gmail.com>
2026-09-20 15:48:39 +0300

    fix word list casing and correct random word selection in Hangman game

--- Day7/project7.py
+++ Day7/project7.py
@@ -1,5 +1,12 @@
 # Hang man game
 # import the random module 
 import random
-word_list = ["Serendipity", "Ephemeral", "Luminescence", "Solitude", "Resilience", "Eloquence", "Melancholy", "Aurora", "Petrichor", "Halcyon"] 
-random_word = random.choices()
+word_list = ["serendipity", "ephemeral", "luminescence", "solitude", "resilience", "eloquence", "melancholy", "aurora", "petrichor", "alcyon"] 
+random_word = random.choice(word_list)
+print (random_word)
+guess = input("Guess a letter ").lower()
+for g in random_word:
+    if g == guess:
+        print("Right") 
+    else:
+        print("Wrong")
