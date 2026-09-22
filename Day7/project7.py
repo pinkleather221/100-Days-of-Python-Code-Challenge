c67f6576b7876c0704b1b7ca97008b531e2197b5
pinkleather221 <nellymogere2002@gmail.com>
2026-09-22 16:37:49 +0300

    refactor Hangman game to enhance game logic and add lives tracking

--- Day7/project7.py
+++ Day7/project7.py
@@ -1,6 +1,91 @@
 # Hang man game
 # import the random module 
 import random
+hangman_stages = [
+    # Stage 6: Both legs added (Game Over)
+    """
+       --------
+
+       |      |
+       |      O
+
+       |     /|\\
+       |      |
+       |     / \\
+    --------
+    """,
+    # Stage 5: One leg added
+    """
+       --------
+
+       |      |
+       |      O
+
+       |     /|\\
+       |      |
+       |     / 
+    --------
+    """,
+    # Stage 4: Both arms added
+    """
+       --------
+
+       |      |
+       |      O
+
+       |     /|\\
+       |      |
+       |     
+    --------
+    """,
+    # Stage 3: One arm added
+    """
+       --------
+
+       |      |
+       |      O
+
+       |     /|
+       |      |
+       |     
+    --------
+    """,
+    # Stage 2: Torso added
+    """
+       --------
+
+       |      |
+       |      O
+
+       |      |
+       |      |
+       |     
+    --------
+    """,
+    # Stage 1: Head added
+    """
+       --------
+
+       |      |
+       |      O
+       |     
+       |      
+       |     
+    --------
+    """,
+    # Stage 0: Base and gallows only
+    """
+       --------
+
+       |      |
+       |      
+       |     
+       |      
+       |     
+    --------
+    """
+]
+lives = 6
 word_list = ["serendipity", "ephemeral", "luminescence", "solitude", "resilience", "eloquence", "melancholy", "aurora", "petrichor", "alcyon"] 
 random_word = random.choice(word_list)
 print (random_word)
@@ -15,6 +100,7 @@ print(placeholder)
 c_guessed = []
 
 game_over = False
+
 while not game_over: 
 # replacing the underscore with the correct guessed letter 
     guess = input("Guess a letter ").lower()
@@ -27,10 +113,22 @@ while not game_over:
             display+= letter
         else:
             display += "_"
+
+        if lives == 0:
+            print()
+    
     print(display)    
 
     # checking to see if the underscore is no longer in the word then printing out "you win " and game_over resets to true 
     if "_" not in display:
         game_over = True
-        print("You got it right, You win!")  
+        print("You got it right, You win!")
+
+    if guess not in random_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You Loose")
+
+    print(hangman_stages[lives])
 
